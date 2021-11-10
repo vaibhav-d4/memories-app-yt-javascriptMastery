a2f8f87e8a5d4c5848a3f2c5dcea9cb1f616e3c2
Dholakia Vaibhav <vdholakia04@gmail.com>
2021-11-10 20:46:53 +0530

    styles ifx

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -1,13 +1,13 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@mui/styles";
 
 export default makeStyles((theme) => ({
   root: {
     "& .MuiTextField-root": {
-      margin: theme.spacing(1),
+      // margin: theme.spacing(1),
     },
   },
   paper: {
-    padding: theme.spacing(2),
+    // padding: theme.spacing(2),
   },
   form: {
     display: "flex",
