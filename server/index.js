c910903c2fe38463fad022beb7cc2ea436d0f4b8
Dholakia Vaibhav <vdholakia04@gmail.com>
2021-11-18 21:23:54 +0530

    completed first part of video

--- server/index.js
+++ server/index.js
@@ -7,21 +7,21 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
+
 const CONNECTION_URL =
-    "mongodb+srv://javascriptmastery:javascriptmastery123@memories-app-yt-javascr.jeuho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://javascriptmastery:javascriptmastery123@memories-app-yt-javascr.jeuho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNECTION_URL)
-    .then(() =>
-        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-    )
-    .catch((error) => console.log(error.message));
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
 // mongoose.set("useFindAndModify", false);
