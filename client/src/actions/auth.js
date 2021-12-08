3fef75d28a7c39c5a2ee8537327e4af228bbb4db
Dholakia Vaibhav <vdholakia04@gmail.com>
2021-12-08 17:17:49 +0530

    JWT Auth, Part 3, 1 hour 26 mins done

---
+++ client/src/actions/auth.js
@@ -0,0 +1,22 @@
+import { AUTH } from "../constants/actionTypes";
+import * as api from "../api/index.js";
+
+export const signin = (formData, navigate) => async (dispatch) => {
+  try {
+    // log in the user
+
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = (formData, navigate) => async (dispatch) => {
+  try {
+    // signup the user
+
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
