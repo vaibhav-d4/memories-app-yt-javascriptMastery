6da418e4c43ebb2bce9cb4acc99cca3ff7af9ae3
Dholakia Vaibhav <vdholakia04@gmail.com>
2021-12-08 22:19:47 +0530

    Netlify Deployment Commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = "https://memories-app-yt-jsmastery.herokuapp.com/posts";
-const url = "http://localhost:5000";
+const url = "https://memories-app-yt-jsmastery.herokuapp.com";
+// const url = "http://localhost:5000";
 
 const API = axios.create({
   baseURL: url,
