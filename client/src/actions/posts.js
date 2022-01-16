75c3ee0832f05bc1c924248c012e85244ae25ed0
Dholakia Vaibhav <vdholakia04@gmail.com>
2022-01-16 17:04:32 +0530

    refs #part-4-pagination-and-search, Loading, upto 1 hour 25 mins done

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -4,14 +4,19 @@ import {
   CREATE,
   UPDATE,
   DELETE,
+  START_LOADING,
+  END_LOADING,
 } from "../constants/actionTypes";
 import * as api from "../api/index.js";
 
 // ACTION CREATORS
 export const getPosts = (page) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
     const { data } = await api.fetchPosts(page);
+
     dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error.message);
   }
@@ -19,10 +24,13 @@ export const getPosts = (page) => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
     const {
       data: { data },
     } = await api.fetchPostsBySearch(searchQuery);
+
     dispatch({ type: FETCH_BY_SEARCH, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error.message);
   }
@@ -30,8 +38,11 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
     const { data } = await api.createPosts(post);
+
     dispatch({ type: CREATE, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
   }
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -7,9 +7,11 @@ import useStyles from "./styles";
 
 const Posts = ({ setCurrentId }) => {
   const classes = useStyles();
-  const { posts } = useSelector((state) => state.posts);
+  const { posts, isLoading } = useSelector((state) => state.posts);
 
-  return !posts?.length ? (
+  if (!posts.length && !isLoading) return "No Posts!";
+
+  return isLoading ? (
     <CircularProgress />
   ) : (
     <Grid
